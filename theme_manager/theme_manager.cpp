b2dd3a25efd424c24ac719326cd1c830eda2f4ec
Azure <azurexsx@gmail.com>
2024-10-30 20:17:47 +0100

    ZURUI_DEBUG definition

--- theme_manager/theme_manager.cpp
+++ theme_manager/theme_manager.cpp
@@ -1,16 +1,102 @@
 #include "theme_manager.h"
 
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QProcess>
+#include <QQmlComponent>
+#include <QQmlContext>
+
 #include "qglobal.h"
 #include "qobject.h"
+#include "qobjectdefs.h"
 
 ThemeManager::ThemeManager(QQmlEngine* engine, QObject* parent)
-    : QObject(parent), m_engine(engine) {
+    : QObject(parent), m_engine(engine), m_current_theme(nullptr) {
+    loadThemes();
 }
 
-void ThemeManager::update() {
+void ThemeManager::loadThemes() {
+    QDir themesDir("themes");
+
+    if (!themesDir.exists()) {
+        qWarning() << "Themes directory does not exist!";
+        return;
+    }
+
+#ifdef ZURUI_DEBUG
+    qDebug() << "Executing command: tree themes";
+#endif
+
+    QProcess process;
+    process.start("tree", QStringList() << "themes");
+    process.waitForFinished();
+
+    QString output = process.readAllStandardOutput();
+    if (!output.isEmpty()) {
+#ifdef ZURUI_DEBUG
+        qDebug() << "Directory structure of 'themes':\n" << output;
+#else
+        qDebug()
+            << "No output from 'tree' command or 'tree' command not found.";
+#endif
+    }
+
+    for (const QFileInfo& dirInfo :
+         themesDir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot)) {
+        QString themeName = dirInfo.fileName();
+        QString themePath = dirInfo.absoluteFilePath() + "/conf.qml";
+
+#ifdef ZURUI_DEBUG
+        qDebug() << "Attempting to load theme:" << themePath;
+#endif
+
+        QObject* themeObject = createThemeObject(themePath);
+        if (themeObject) {
+            m_themes.insert(themeName, QVariant::fromValue(themeObject));
+        }
+    }
+}
+
+QVariantMap ThemeManager::availableThemes() const {
+    return m_themes;
+}
+
+QObject* ThemeManager::createThemeObject(const QString& path) {
+    if (!QFile::exists(path)) {
+        qWarning() << "Theme file does not exist at path:" << path;
+        return nullptr;
+    }
+
+    QQmlComponent component(m_engine, QUrl::fromLocalFile(path));
+    if (component.status() != QQmlComponent::Ready) {
+        qWarning() << "Failed to load theme:" << component.errorString();
+        return nullptr;
+    }
+
+    QObject* themeObject = component.create();
+    if (!themeObject) {
+        qWarning() << "Failed to instantiate theme object from QML file";
+    }
+    return themeObject;
+}
+
+void ThemeManager::update(const QString& theme_name) {
+    if (!m_themes.contains(theme_name)) {
+        qWarning() << "Theme not found:" << theme_name;
+        return;
+    }
+
+    m_current_theme = m_themes[theme_name].value<QObject*>();
+    if (!m_current_theme) {
+        qWarning() << "Failed to instantiate theme object from QML file";
+        return;
+    }
+
+    emit currentThemeChanged();
 }
 
 ThemeManager::~ThemeManager() {
-    if (m_engine)
-        delete m_engine;
+    if (m_current_theme)
+        delete m_current_theme;
 }
