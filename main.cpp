d2ed3561bfcc953a192a8e6b90709fc556deadb5
Azure <azurexsx@gmail.com>
2024-11-26 23:55:22 +0100

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,67 @@
 
+#include <QApplication>
 #include <QDir>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QWindow>
 #include <QtQml/QQmlExtensionPlugin>
+#include <cmath>
+#include <cstdio>
 
 #include "key_bindings/key_binding_manager.h"
 #include "preloading_manager/preloading_manager.h"
+#include "qdebug.h"
+#include "qnamespace.h"
+#include "qplugin.h"
 #include "qqml.h"
-#include "surface_manager/surface.h"
 #include "surface_manager/surface_manager.h"
+#include "systray/systray.h"
 #include "theme_manager/theme_manager.h"
 #include "translation_tools/translation_manager.h"
-// Q_IMPORT_QML_PLUGIN(UIcomponentsPlugin)
-// Q_IMPORT_QML_PLUGIN(TranslationToolsPlugin)
-// Q_IMPORT_QML_PLUGIN(ThemeManagerPlugin)
 
-/*!
- * \brief Overview of Plugin-Based Approach
- *
- * This application employs a plugin-based architecture to promote modularity
- * and flexibility. Each plugin represents a specific functionality or feature,
- * such as themes or UI components, allowing developers to extend or modify the
- * project without altering the core logic.
- *
- * Note: Importing plugins directly with `Q_IMPORT_QML_PLUGIN()` can increase
- * RAM usage during the build process, especially when plugins include
- * large-sized assets. Use with consideration to optimize memory consumption
- * during compilation.
- */
-// Q_IMPORT_QML_PLUGIN(Zurui_lightPlugin)
+/*
+    Note: From now on, application will use mostly dynamic linking,
+    so please consider avoiding implicit static linking of any modules.
+*/
+Q_IMPORT_PLUGIN(TranslationToolsPlugin)
 
 int main(int argc, char* argv[]) {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    KeyBindingManager keyBindingManager(&engine);
-
+    engine.addImportPath(":/");
     engine.addImportPath("qrc:/ui_components");
-    app.installEventFilter(&keyBindingManager);
 
-    // const auto importPathList = engine.importPathList();
-    // qDebug() << "QML Import Paths:";
-    // for (const QString& path : importPathList) {
-    //     qDebug() << path;
-    // }
+#ifdef ZURUI_DEBUG
+    QDir resourceDir(":/");
+    if (!resourceDir.exists()) {
+        qDebug() << "The directory does not exist.";
+    }
+
+    QStringList entries =
+        resourceDir.entryList(QDir::Files | QDir::NoDotAndDotDot);
+    for (const QString& entry : entries) {
+        qDebug() << "File:" << entry;
+    }
+#endif
+
 #ifdef ZURUI_DEBUG
     engine.rootContext()->setContextProperty("zurui_debug", true);
 #else
     engine.rootContext()->setContextProperty("zurui_debug", false);
 #endif
 
+    // Context props
     ThemeManager themeManager(&engine);
     TranslationManager translationManager(&engine);
     PreloadingManager preloadingManager(&engine);
+    KeyBindingManager keyBindingManager(&engine);
+    translationManager.changeLanguage("ja");
+    qDebug() << "Current language: [" << translationManager.currentLanguage()
+             << "]";
+    SysTray sysTray(&app);
+    app.installEventFilter(&keyBindingManager);
 
     engine.rootContext()->setContextProperty("themeManager", &themeManager);
     engine.rootContext()->setContextProperty("keyBindingManager",
@@ -63,6 +70,7 @@ int main(int argc, char* argv[]) {
                                              &translationManager);
     engine.rootContext()->setContextProperty("preloadingManager",
                                              &preloadingManager);
+
     themeManager.update("zurui_dark");
 
     QObject::connect(
